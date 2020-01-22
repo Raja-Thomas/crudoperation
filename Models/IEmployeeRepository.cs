5126ad9a77eb17e5b446653ad38bb677aaad9811
Raja-Thomas <60168092+Raja-Thomas@users.noreply.github.com>
2020-01-22 14:59:41 +0530

    Add project files.

---
+++ Models/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using razorpages.Models;
+using System.Collections.Generic;
+
+namespace razorpages.Services
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAllEmployees();
+        List<Cars> GetAll();
+    }
+}
---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using razorpages.Models;
+using razorpages.Services;
+using System.Collections.Generic;
+
+namespace razorpages.Services
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = Dept.HR,
+                    Email = "mary@pragimtech.com", PhotoPath= "mary.jpg" },
+                new Employee() { Id = 2, Name = "John", Department = Dept.IT,
+                    Email = "john@pragimtech.com", PhotoPath="john.jpg" },
+                new Employee() { Id = 3, Name = "Sara", Department = Dept.IT,
+                    Email = "sara@pragimtech.com", PhotoPath="sara.jpg" },
+                new Employee() { Id = 4, Name = "David", Department = Dept.Payroll,
+                    Email = "david@pragimtech.com" },
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+        public List<Cars> GetAll()
+        {
+            List<Cars> cars = new List<Cars>{
+            new Cars{Id = 1, Make="Audi",Model="R8",Year=2014,Doors=2,Colour="Red",Price=79995},
+            new Cars{Id = 2, Make="Aston Martin",Model="Rapide",Year=2010,Doors=2,Colour="Black",Price=54995},
+            new Cars{Id = 3, Make="Porsche",Model=" 911 991",Year=2016,Doors=2,Colour="White",Price=155000},
+            new Cars{Id = 4, Make="Mercedes-Benz",Model="GLE 63S",Year=2017,Doors=5,Colour="Blue",Price=83995},
+            new Cars{Id = 5, Make="BMW",Model="X6 M",Year=2016,Doors=5,Colour="Silver",Price=62995},
+        };
+            return cars;
+        }
+    }
+}
